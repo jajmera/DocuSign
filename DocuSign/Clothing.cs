7d08196e72f140fe4f3f21f72e10bb28d984e0c5
jatan <jka292@nyu.edu>
2016-04-06 22:25:06 -0400

    First Commit

---
+++ DocuSign/Clothing.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace DocuSign
+{
+	class Clothing
+	{
+		string category;
+		int command;
+		string hot;
+		string cold;
+
+
+		public void set(string newCategory, int command, string hot, string cold)
+		{
+			this.category = newCategory;
+			this.command = command;
+			this.hot = hot;
+			this.cold = cold;
+		}
+
+		public string getCategory()
+		{
+			return this.category;
+		}
+		public int getCommand()
+		{
+			return this.command;
+		}
+
+		public string getName(String weather)
+		{
+
+			if (weather.Equals("hot"))
+				return this.hot;
+			else
+				return this.cold;
+		}
+
+		public string getHot()
+		{
+			return this.hot;
+		}
+		public string getCold()
+		{
+			return this.cold;
+		}
+	}
+
+}
+
---
+++ DocuSign/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DocuSign
+{
+	class MainClass
+	{
+		
+		public static void Main (string[] args)
+		{
+			Console.WriteLine ("Hello World!");
+
+
+			Clothing footwear = new Clothing();
+			Clothing headwear = new Clothing();
+			Clothing socks = new Clothing();
+			Clothing shirt = new Clothing();
+			Clothing jacket = new Clothing();
+			Clothing pants = new Clothing();
+
+			//initialize the values of the clothing and the commands
+			footwear.set("footwear", 1,"sandals","boots");
+			headwear.set("headwear", 2,"sun visor","hat");
+			socks.set("socks", 3,"fail","socks");
+			shirt.set("shirt", 4,"t-shirt","shirt");
+			jacket.set("jacket", 5,"fail","jacket");
+			pants.set("pants", 6,"shorts","pants");
+
+
+			List<Clothing> options = new List<Clothing>();
+
+			//Add the clothing items to a list to pass it to the function
+			options.Add(footwear);
+			options.Add(headwear);
+			options.Add(socks);
+			options.Add(shirt);
+			options.Add(jacket);
+			options.Add(pants);
+
+			//initialize commands
+			int[] commands={8, 6, 6};
+			//initialize weather
+			string weather = "hot";
+
+			checkRules(commands,options,weather);
+
+		}
+
+		static void checkRules(int [] commands, List<Clothing> options ,string weather )
+		{
+			HashSet<string> previous = new HashSet<string>(); 
+			// if first command not to remove pyjamas --> fail
+			if(commands[0]!= 8)
+				return ;
+			Console.Write ("Removing PJ's, ");
+			int i;
+			for( i=1;(i<commands.Length && (commands[i]!=7));i++)
+			{
+				int current = commands[i];
+
+				//Check validity of command
+				if (current > 6 || current < 1) {
+					Console.WriteLine ("fail");
+					return;
+				}
+
+				Clothing cloth = options[commands[i]-1];
+				string category = cloth.getCategory();
+
+				if (previous.Contains (category)) {
+					Console.WriteLine ("fail");
+					return ;
+				}
+				else
+				{
+					if (weather.Equals ("hot")) {
+						if (category.Equals ("socks") || category.Equals ("jacket")) {
+
+							Console.WriteLine ("fail");
+							return;
+						}
+
+					} else if (category.Equals ("footear") && !previous.Contains ("socks")) {
+						Console.WriteLine ("fail");
+						return;
+					}
+					if (category.Equals ("headwear") && !previous.Contains ("shirt")) {
+						Console.WriteLine ("fail");
+						return ;
+					}
+					previous.Add(category);
+					Console.Write (cloth.getName(weather));
+					Console.Write (", ");
+				}
+
+			}
+
+				//Check if all clothing is worn before leaving
+			if (weather.Equals("cold")){
+				if (previous.Count != 6 || i >= commands.Length) {
+					
+					Console.WriteLine ("fail");
+						return;
+				}
+				 else
+					Console.WriteLine ("leaving house");
+					return;
+			}
+
+			else if (weather.Equals("hot") ) {
+				if(previous.Count!=4 || i>=commands.Length){
+					
+				Console.WriteLine ("fail");
+						return;
+			} else
+				Console.WriteLine ("leaving house");
+					return;
+		
+			}
+				else
+				return;
+
+	}
+}
+}
